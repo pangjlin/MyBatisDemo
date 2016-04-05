e592fe9703e08a11c5a9f6ac04a0ed77f88a09b5
PANGJIANLIN <PANGJIANLIN@kingsoft.com>
2016-04-05 16:15:25 +0800

    2016.04.05

--- src/main/java/com/ksc/mybatis/dao/StudentMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ksc.mybatis.dao;
-
-import com.ksc.mybatis.controller.Student;
-
-public interface StudentMapper {
-	Student findStudentById(Integer id); 
-	void insertStudent(Student student); 
-}
